7b4f9cf704d6e98d8e9b1ba0248e7cd8f0bde97c
vinicius Lima <vinicius.limalveslinux@gmail.com>
2023-02-28 22:22:05 -0300

    Implementado um javadoc e comdificado como é instanciado o objeto na main

--- CadastroPessoas/cadastro/Array.java
+++ CadastroPessoas/cadastro/Array.java
@@ -1,10 +1,18 @@
 package CadastroPessoas.cadastro;
 
 public class Array {
-    private static Pessoa [] lista = new Pessoa[3];
+    private static Pessoa [] lista = new Pessoa[3000000];
     private static int indice = 0;
 
+    /**
+     * Realiza inserção de objetos do tipo Pessoa no
+     * Array List
+     * @param p
+     */
     public static void inserir(Pessoa p){
+
+        p.setId(indice+1);
+
         lista[indice++] = p;     
     }
 
--- CadastroPessoas/cadastro/CadastroPessoas.java
+++ CadastroPessoas/cadastro/CadastroPessoas.java
@@ -2,11 +2,9 @@ package CadastroPessoas.cadastro;
 
 public class CadastroPessoas {
     public static void main(String[] args) {
-        Pessoa p1 = new Pessoa("Vinicius", 24);
-        Pessoa p2 = new Pessoa("Leonardo", 19);
-
-        Array.inserir(p1);
-        Array.inserir(p2);
+        
+        Array.inserir(new Pessoa("Vinicius", 24));
+        Array.inserir(new Pessoa("Leonardo", 19));
 
         Array.mostrar();
 
